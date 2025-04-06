143ff92d86dedc0800761c3821f13cf1225b832f
Beyza Nur Çelebi <beyzanrcelebii@gmail.com>
2025-04-06 18:42:09 +0300

    Kullanici giris sayfasi eklendi

--- app/src/main/java/com/beyzanurcelebi/kullanicigiris/MainActivity.kt
+++ app/src/main/java/com/beyzanurcelebi/kullanicigiris/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.beyzanurcelebi.kullanicigiris
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
