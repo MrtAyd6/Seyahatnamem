5bb61883cf90b501ac2ff1bab1fa4621b5fb65fb
MrtAyd6 <mrmurat5757@gmail.com>
2025-05-01 22:26:58 +0300

    nav_graph düzenlendi

---
+++ app/src/main/java/com/example/seyahatnamem/GeziDetayFragment.kt
@@ -0,0 +1,27 @@
+package com.example.seyahatnamem
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class GeziDetayFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gezi_detay, container, false)
+    }
+
+
+}
